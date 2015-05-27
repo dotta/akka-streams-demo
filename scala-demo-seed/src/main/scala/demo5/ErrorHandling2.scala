379e5f23b13b08040a5a7232b6fd28e34a55ebcf
Mirco Dotta <mirco.dotta@gmail.com>
2015-05-27 15:27:32 +0200

    Added one extra comment in ErrorHandling2.scala

--- scala-demo-seed/src/main/scala/demo5/ErrorHandling2.scala
+++ scala-demo-seed/src/main/scala/demo5/ErrorHandling2.scala
@@ -13,6 +13,7 @@ object ErrorHandling2 extends App {
   implicit val system = ActorSystem("demo5")
   import system.dispatcher
 
-  // Create a materializer with a supervision strategy that Resume if an ArithmeticException occurs
+  // Create a materializer with a supervision strategy that Resume if an ArithmeticException occurs.
+  // The superision strategy is applied to the whole stream.
 
 }
